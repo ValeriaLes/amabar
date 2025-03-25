c320857b6a8f92b29dbf0d5862781b6f061aae31
Valeria <valeria.l.1994outlook.com>
2025-03-26 00:23:20 +0100

    changed main picture, changed size about page, added new photos, fixed footer to make phone email clickable

--- main.js
+++ main.js
@@ -40,4 +40,68 @@ document.addEventListener("DOMContentLoaded", function () {
     //   bodyScrollLock.enableBodyScroll(document.body);
     });
 
- 
+
+    // document.addEventListener("DOMContentLoaded", function () {
+    //   const galleryData = {
+    //     bar: ["cocktail1.jpg", "cocktail2.jpg", "cocktail3.jpg", "cocktail4.jpg"],
+    //     shisha: ["shisha1.jpg", "shisha2.jpg", "shisha3.jpg", "shisha3.jpg"],
+    //     karaoke: ["karaoke7.jpg", "karaoke8.jpg", "karaoke4.jpg", "karaoke4.jpg"],
+    //     gaming: ["gaming.png", "gaming4.jpg", "gaming4.jpg", "gaming4.jpg"],
+    //   };
+    
+    //   const lightbox = document.getElementById("lightbox");
+    //   const lightboxImages = document.getElementById("lightbox-images");
+    //   const closeLightbox = document.querySelector(".close-lightbox");
+    
+    //   let swiperInstance = null;
+    
+    //   document.querySelectorAll(".open-gallery").forEach((item) => {
+    //     item.addEventListener("click", function () {
+    //       const category = this.dataset.category;
+    //       lightboxImages.innerHTML = "";
+    
+    //       // Bilder aus der galleryData in den Swiper Wrapper einfügen
+    //       galleryData[category].forEach((img) => {
+    //         const slide = document.createElement("div");
+    //         slide.classList.add("swiper-slide");
+    //         slide.innerHTML = `<img class="swiper-lazy" data-src="./images/${img}" alt="${category}">`;
+    //         lightboxImages.appendChild(slide);
+    //       });
+    
+    //       lightbox.style.display = "flex";
+    
+    //       // Zerstöre die vorherige Swiper-Instanz, falls sie existiert
+    //       if (swiperInstance) swiperInstance.destroy();
+    
+    //       swiperInstance = new Swiper(".swiper-lightbox", {
+    //         navigation: {
+    //           nextEl: ".swiper-button-next",
+    //           prevEl: ".swiper-button-prev",
+    //         },
+    //         pagination: {
+    //           el: ".swiper-pagination",
+    //           clickable: true,
+    //         },
+    //         loop: true,
+    //         loopedSlides: galleryData[category].length,
+    //         slidesPerView: 1,
+    //         slidesPerGroup: 1,
+    //         centeredSlides: true,
+    //         keyboard: {
+    //           enabled: true,
+    //         },
+    //         lazy: true, 
+    //       });
+    //     });
+    //   });
+    
+    //   closeLightbox.addEventListener("click", function () {
+    //     lightbox.style.display = "none";
+    //   });
+    
+    //   lightbox.addEventListener("click", function (e) {
+    //     if (e.target === lightbox) {
+    //       lightbox.style.display = "none";
+    //     }
+    //   });
+    // });
