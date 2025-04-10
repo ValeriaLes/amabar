5af2bda0ce5b3647cd0ba9301dc6e232a5d33962
Valeria <valeria.l.1994outlook.com>
2025-04-10 06:28:19 +0200

    added cname datei

--- main.js
+++ main.js
@@ -25,94 +25,26 @@ document.addEventListener("DOMContentLoaded", function () {
       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
       mobileMenu.classList.toggle('is-open');
   
-    //   const scrollLockMethod = !isMenuOpen
-    //     ? 'disableBodyScroll'
-    //     : 'enableBodyScroll';
-    //   bodyScrollLock[scrollLockMethod](document.body);
     };
   
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
   
-    // Close the mobile menu on wider screens if the device orientation changes
+ 
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
       mobileMenu.classList.remove('is-open');
       openMenuBtn.setAttribute('aria-expanded', false);
-    //   bodyScrollLock.enableBodyScroll(document.body);
+   
     });
 
 
-    // document.addEventListener("DOMContentLoaded", function () {
-    //   const galleryData = {
-    //     bar: ["cocktail1.jpg", "cocktail2.jpg", "cocktail3.jpg", "cocktail4.jpg"],
-    //     shisha: ["shisha1.jpg", "shisha2.jpg", "shisha3.jpg", "shisha3.jpg"],
-    //     karaoke: ["karaoke7.jpg", "karaoke8.jpg", "karaoke4.jpg", "karaoke4.jpg"],
-    //     gaming: ["gaming.png", "gaming4.jpg", "gaming4.jpg", "gaming4.jpg"],
-    //   };
-    
-    //   const lightbox = document.getElementById("lightbox");
-    //   const lightboxImages = document.getElementById("lightbox-images");
-    //   const closeLightbox = document.querySelector(".close-lightbox");
-    
-    //   let swiperInstance = null;
-    
-    //   document.querySelectorAll(".open-gallery").forEach((item) => {
-    //     item.addEventListener("click", function () {
-    //       const category = this.dataset.category;
-    //       lightboxImages.innerHTML = "";
-    
-    //       // Bilder aus der galleryData in den Swiper Wrapper einfügen
-    //       galleryData[category].forEach((img) => {
-    //         const slide = document.createElement("div");
-    //         slide.classList.add("swiper-slide");
-    //         slide.innerHTML = `<img class="swiper-lazy" data-src="./images/${img}" alt="${category}">`;
-    //         lightboxImages.appendChild(slide);
-    //       });
-    
-    //       lightbox.style.display = "flex";
-    
-    //       // Zerstöre die vorherige Swiper-Instanz, falls sie existiert
-    //       if (swiperInstance) swiperInstance.destroy();
-    
-    //       swiperInstance = new Swiper(".swiper-lightbox", {
-    //         navigation: {
-    //           nextEl: ".swiper-button-next",
-    //           prevEl: ".swiper-button-prev",
-    //         },
-    //         pagination: {
-    //           el: ".swiper-pagination",
-    //           clickable: true,
-    //         },
-    //         loop: true,
-    //         loopedSlides: galleryData[category].length,
-    //         slidesPerView: 1,
-    //         slidesPerGroup: 1,
-    //         centeredSlides: true,
-    //         keyboard: {
-    //           enabled: true,
-    //         },
-    //         lazy: true, 
-    //       });
-    //     });
-    //   });
-    
-    //   closeLightbox.addEventListener("click", function () {
-    //     lightbox.style.display = "none";
-    //   });
-    
-    //   lightbox.addEventListener("click", function (e) {
-    //     if (e.target === lightbox) {
-    //       lightbox.style.display = "none";
-    //     }
-    //   });
-    // });
 
     document.addEventListener("DOMContentLoaded", function () {
-      // GLightbox initialisieren
+    
     
   
-      // Event-Listener für Kategorien
+    
       const categoryButtons = document.querySelectorAll(".open-gallery");
       categoryButtons.forEach(button => {
           button.addEventListener("click", function () {
@@ -122,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
   
-  // Die Funktion, die die Galerie basierend auf der Kategorie öffnet
+  
   function openGallery(category) {
     const galleryImages = {
       bar: ["cocktail1.jpg", "cocktail2.jpg", "cocktail3.jpg","cocktail4.jpg","cocktail5.jpg","cocktail6.jpg","cocktail7.jpg","cocktail8.jpg", "cocktail9.jpg","cocktail10.jpg","cocktail11.jpg","cocktail12.jpg","cocktail13.jpg","cocktail14.jpg","cocktail16.jpg","cocktail17.jpg","cocktail19.jpg","cocktail-video.mp4","cocktail-video2.mp4"],
@@ -132,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 
   
-      // Erstellen der HTML-Markup für die Galerie
+    
       const imagesHtml = galleryImages[category].map(file => {
           const filePath = `./images/${file}`;
           const isVideo = file.endsWith(".mp4");
@@ -146,10 +78,10 @@ document.addEventListener("DOMContentLoaded", function () {
                  </a>`;
       }).join("");
   
-      // Füge das HTML der Galerie in den Container ein
+   
       document.getElementById("gallery-container").innerHTML = imagesHtml;
   
-      // GLightbox aktualisieren, nachdem die Galerie geladen wurde
+     
       const updatedLightbox = GLightbox({
           selector: ".glightbox",
           autoplayVideos: true,
@@ -157,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
           autoplayVideos: true,
           navigation: true,
           onOpen: () => {
-            // Wenn die Lightbox geöffnet wird, Vollbild aktivieren
+            
             const videoElement = document.querySelector(".gpopup video");
             if (videoElement) {
                 openFullscreen(videoElement);
@@ -167,15 +99,14 @@ document.addEventListener("DOMContentLoaded", function () {
       function openFullscreen(video) {
         if (video.requestFullscreen) {
             video.requestFullscreen();
-        } else if (video.mozRequestFullScreen) { // Firefox
+        } else if (video.mozRequestFullScreen) { 
             video.mozRequestFullScreen();
-        } else if (video.webkitRequestFullscreen) { // Chrome, Safari und Opera
-            video.webkitRequestFullscreen();
-        } else if (video.msRequestFullscreen) { // IE/Edge
+        } else if (video.webkitRequestFullscreen) { 
+        } else if (video.msRequestFullscreen) { 
             video.msRequestFullscreen();
         }
     }
   
-      // Lightbox öffnen
+      
       updatedLightbox.open();
   }
